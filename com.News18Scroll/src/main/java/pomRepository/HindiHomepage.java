0814f63bb5db74e8f0c9b500ad2e1ddabad74b4e
shubh114 <shubhamgupta110494@gmail.com>
2021-12-14 08:04:44 +0530

    kkk

--- com.News18Scroll/src/main/java/pomRepository/HindiHomepage.java
+++ com.News18Scroll/src/main/java/pomRepository/HindiHomepage.java
@@ -9,6 +9,7 @@ import utility.WebdriverUtility;
 
 public class HindiHomepage extends WebdriverUtility {
 	WebDriver driver;
+	
 	public HindiHomepage(WebDriver driver) {
 		this.driver=driver;
 		PageFactory.initElements(driver, this);
@@ -16,8 +17,13 @@ public class HindiHomepage extends WebdriverUtility {
 	@FindBy(xpath="//div[@class='jsx-2997171482 globalhd large dflex justify-space-betwwen']//a[.=\"मनी\"]")
 	private WebElement Money;
 	
+	@FindBy(xpath="//ul[@class='jsx-3482196704 naukrinews-left']li")
+	private WebElement MoneyCon;
 	
-//	@FindBy(xpath="//div[@class='jsx-2997171482 globalhd large dflex justify-space-betwwen']//a[.=\"मनी\"]")
-//	private WebElement Mony;
+	@FindBy(xpath="//ul[@class='jsx-3482196704 naukrinews-left']/li[\"+i+\"]//h2")
+	private WebElement MoneyContent;
+	
+	@FindBy(xpath="//ul[@class='jsx-3482196704 naukrinews-left']/li[\"+i+\"]//a")
+	private WebElement MoneyContentLink;
 
 }
